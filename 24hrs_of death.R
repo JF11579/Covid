055ea7053739c2419e10027aa09263fb7672e65f
Joe Foley <joefoley@MacBook-Pro.localdomain>
2020-04-09 13:23:35 -0400

    Started Bar Graphs

---
+++ 24hrs_of death.R
@@ -0,0 +1,6 @@
+
+# NYS DO_Health  https://covid19tracker.health.ny.gov/views/NYS-COVID19-Tracker/NYSDOHCOVID-19Tracker-Map?%3Aembed=yes
+
+# intersting data  https://covid19tracker.health.ny.gov/views/NYS-COVID19-Tracker/NYSDOHCOVID-19Tracker-TableView?%3Aembed=yes
+
+#https://www.cidrap.umn.edu/news-perspective/2020/04/new-york-sees-record-1-day-rise-covid-19-deaths
